1c09cb0a8702c3bfe3d8d83032933a96f5149df0
bergmacedo <gutemberg.dev@hotmail.com>
2024-09-28 12:11:28 -0300

    projeto starbucks

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+let circulo = document.querySelector('.circulo')
+let imagem = document.querySelector('.copo')
+
+console.log('circulo')
+
+function cliquenobotao(cor) {
+    console.log(cor)
+}
+
+function cliquenobotao(cor) {
+    circulo.style.background = cor
+
+}
+
+function TrocarImagem(endereco) {
+    imagem.src = endereco
+}
